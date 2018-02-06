2efd56912a5801278c37a833ab9d3c81be352be8
Abhinav Gupta <abhinav3006gupta@gmail.com>
2018-02-07 01:40:04 +0530

    #5

--- models/user.js
+++ models/user.js
@@ -11,13 +11,13 @@ const validate = require('mongoose-validator');   // Import Mongoose Validator P
 const nameValidator = [
     validate({
         validator: 'matches',
-        arguments: /^(([a-zA-Z]{3,20})+[ ]+([a-zA-Z]{3,20})+)+$/,
-        message: 'Name must be at least 3 characters, max 30, no special characters or numbers, must have space in between name.'
+        arguments: /^(([a-zA-Z]{3,50})+$/,
+        message: 'Name must be at least 3 characters, max 50, no special characters or numbers.'
     }),
     validate({
         validator: 'isLength',
-        arguments: [3, 20],
-        message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters'
+        arguments: [3, 50],
+        message  :  'Name should be between {ARGS[0]} and {ARGS[1]} characters'
     })
 ];
 
@@ -26,25 +26,12 @@ const emailValidator = [
     validate({
         validator: 'matches',
         arguments: /^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/,
-        message: 'Name must be at least 3 characters, max 40, no special characters or numbers, must have space in between name.'
+        message: 'Name must be at least 3 characters, max 50, no special characters or numbers, must have space in between name.'
     }),
     validate({
         validator: 'isLength',
-        arguments: [3, 40],
+        arguments: [3, 50],
         message: 'Email should be between {ARGS[0]} and {ARGS[1]} characters'
-    })
-];
-
-// Username Validator
-const usernameValidator = [
-    validate({
-        validator: 'isLength',
-        arguments: [3, 25],
-        message: 'Username should be between {ARGS[0]} and {ARGS[1]} characters'
-    }),
-    validate({
-        validator: 'isAlphanumeric',
-        message: 'Username must contain letters and numbers only'
     })
 ];
 
@@ -52,8 +39,8 @@ const usernameValidator = [
 const passwordValidator = [
     validate({
         validator: 'matches',
-        arguments: /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{8,35}$/,
-        message: 'Password needs to have at least one lower case, one uppercase, one number, one special character, and must be at least 8 characters but no more than 35.'
+        arguments: /^(?=.*?[\d]).{8,35}$/,
+        message: 'Password needs to have at least one number and must be at least 8 characters but no more than 35.'
     }),
     validate({
         validator: 'isLength',
@@ -64,28 +51,24 @@ const passwordValidator = [
 
 // User Mongoose Schema
 const UserSchema = new Schema({
-    name: { type: String, required: true, validate: nameValidator },
-    username: { type: String, lowercase: true, required: true, unique: true, validate: usernameValidator },
-    password: { type: String, required: true, validate: passwordValidator, select: false },
-    email: { type: String, required: true, lowercase: true, unique: true, validate: emailValidator },
-    active: { type: Boolean, required: true, default: false },
-    temporarytoken: { type: String, required: true },
-    resettoken: { type: String, required: false },
-    permission: { type: String, required: true, default: 'moderator' },
-    organization:{type:String, required:true}
+    name            :    { type: String,  required: true, validate: nameValidator },
+    password        :    { type: String,  required: true, validate: passwordValidator, select: false },
+    email           :    { type: String,  required: true, validate: emailValidator   , unique: true },
+    permission      :    { type: Boolean, required: true, default: false },
+    temporarytoken  :    { type: String,  required: true },
+    organization    :    { type: String,  required: true }
 });
 
 // Middleware to ensure password is encrypted before saving user to database
 UserSchema.pre('save', function(next) {
     const user = this;
 
-    if (!user.isModified('password')) return next(); // If password was not changed or is new, ignore middleware
-
     // Function to encrypt password
     bcrypt.hash(user.password, null, null, function(err, hash) {
-        if (err) return next(err); // Exit if error is found
-        user.password = hash; // Assign the hash to the user's password so it is saved in database encrypted
-        next(); // Exit Bcrypt function
+        if (err)
+            return next(err);   // Exit if error is found
+        user.password = hash;   // Assign the hash to the user's password so it is saved in database encrypted
+        next();                 // Exit Bcrypt function
     });
 });
 
